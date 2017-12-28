d96154171a0ceaf29a636e3b9971b1fa7934b80f
sekadu <sebastian.kaiser@digitalunit.berlin>
2017-12-28 20:02:41 +0100

    Simplify lambda api decorator

--- src/decorators/lambda/lambda.api.ts
+++ src/decorators/lambda/lambda.api.ts
@@ -4,17 +4,18 @@ import * as DI from '../../di';
 const d = Debug('@Api');
 
 export const api = (config) => {
-  return (target: any) => {
+  return (target) => {
     // import
     d('Running decorator on:', target.prototype, config);
 
-    // create a new instance of App class and store it in DI for future ref
-    target.prototype.DI = DI;
     // target.prototype['factories'] = DI.getFactories();
-    target.prototype.services = DI.getServices();
+    target.prototype = {
+      // create a new instance of App class and store it in DI for future ref
+      DI,
+      services: DI.getServices(),
+    };
 
     d('app prototype:', target.prototype);
     // instantiate Factories and Services
   };
 };
-
